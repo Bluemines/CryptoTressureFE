f3446361f32baae12d39c02da065f4a0ace3a9ba
Jawad Awan <101397741+jawad989@users.noreply.github.com>
2025-04-24 00:17:29 +0500

    auth screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-
-export default function Home() {
-  return (
-    <div className="">
-      
-    </div>
-  );
+import { redirect } from 'next/navigation'
+export default function Home() {  
+  redirect('/login')
 }
