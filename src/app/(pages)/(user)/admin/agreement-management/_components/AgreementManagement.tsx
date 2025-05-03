62623b55bfed930dd4b468a5a03fd91968de383d
Jawad Awan <101397741+jawad989@users.noreply.github.com>
2025-05-04 01:21:06 +0500

    pages

---
+++ src/app/(pages)/(user)/admin/agreement-management/_components/AgreementManagement.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useState } from "react"
+import ReactQuill from "react-quill-new"
+import "react-quill/dist/quill.snow.css"
+
+const AgreementManagement = () => {
+  const [value, setValue] = useState("")
+
+  return (
+    <div className="h-[70vh] bg-[#262626] text-white p-4">
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        className="h-[90%]"
+      />
+    </div>
+  )
+}
+
+export default AgreementManagement
