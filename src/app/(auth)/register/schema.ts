b22c0d0c79b81519faa388acc548fceb7d58e1af
SobanRauf <sobanqu@gmail.com>
2025-05-05 00:34:07 +0500

    build

---
+++ src/app/(auth)/register/schema.ts
@@ -0,0 +1,17 @@
+// validation/registerSchema.ts
+import * as yup from "yup";
+
+export const registerSchema = yup.object({
+  username: yup.string().required("Username is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  code: yup.string().required("Code is required"),
+  password: yup.string().min(6, "Password must be at least 6 characters"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match")
+    .required("Confirm Password is required"),
+  referralCode: yup.string().optional(),
+});
+
+// Type for the form
+export type RegisterFormType = yup.InferType<typeof registerSchema>;
