def10a57aacd52ac316ba519362c69da9b7d20e2
SobanRauf <sobanqu@gmail.com>
2025-05-08 02:08:18 +0500

    created build and admin APIs

---
+++ .next/static/chunks/src_app_(pages)_(user)_admin_dashboard_page_tsx_cc38181b._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_50ea729f._.js",
+  "static/chunks/node_modules_next_0b3364e4._.js",
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_f9867531._.js",
+  "static/chunks/node_modules_@mui_material_esm_0284ee22._.js",
+  "static/chunks/node_modules_axios_lib_99999129._.js",
+  "static/chunks/node_modules_adef9c4c._.js"
+],
+    source: "dynamic"
+});
