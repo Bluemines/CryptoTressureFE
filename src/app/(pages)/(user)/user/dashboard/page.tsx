0135f6e74e63ccf89b6ef9ba510270f306237d81
Jawad Awan <101397741+jawad989@users.noreply.github.com>
2025-05-12 22:59:13 +0500

    fix

--- src/app/(pages)/(user)/user/dashboard/page.tsx
+++ src/app/(pages)/(user)/user/dashboard/page.tsx
@@ -44,47 +44,8 @@ const page = () => {
     },
   ];
 
-  const nftData = [
-    {
-      image: "https://images.unsplash.com/photo-1531297484001-80022131f5a1",
-      title: "Distant Galaxy",
-      price: 1000,
-      dailyIncome: 2,
-      fee: 1,
-      days: 10,
-      level: "Lv1-Lv3",
-    },
-    {
-      image: "https://images.unsplash.com/photo-1581091226825-a6a2a5aee158",
-      title: "Distant Galaxy",
-      price: 1000,
-      dailyIncome: 2,
-      fee: 1,
-      days: 10,
-      level: "Lv1-Lv3",
-    },
-    {
-      image: "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5",
-      title: "Distant Galaxy",
-      price: 1000,
-      dailyIncome: 2,
-      fee: 1,
-      days: 10,
-      level: "Lv1-Lv3",
-    },
-    {
-      image: "https://images.unsplash.com/photo-1500673922987-e212871fec22",
-      title: "Distant Galaxy",
-      price: 1000,
-      dailyIncome: 2,
-      fee: 1,
-      days: 10,
-      level: "Lv1-Lv3",
-    },
-  ];
-
   return (
-    <div className='min-h-screen bg-black p-4 md:p-8'>
+    <div className='min-h-screen bg-black py-4 md:p-8'>
       <div className='max-w-7xl mx-auto space-y-8'>
         {/* Stats Grid */}
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
